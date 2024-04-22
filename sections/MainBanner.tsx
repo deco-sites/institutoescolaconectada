ed8cf7b7395c786c4ec4d74f81093727eab52503
Rodrigo Araujo <rodrigo.eritel@gmail.com>
2024-04-22 08:19:47 -0300

    feat: finish banner main

--- sections/MainBanner.tsx
+++ sections/MainBanner.tsx
@@ -1,3 +1,4 @@
+// import { useState } from 'preact/hooks';
 import Image from "apps/website/components/Image.tsx";
 import type { ImageWidget } from "apps/admin/widgets.ts";
 
@@ -36,7 +37,7 @@ export interface Props {
 
 export default function BannerPrincipal({
     imagem = {
-        src: "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/1527/67120bcd-936a-4ea5-a760-02ed5c4a3d04",
+        src: "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/7759/f941a0e0-1296-4e1f-993a-afbdea727685",
         alt: "Banner Principal - Instituto Escola Conectada"
     },
     imagemPlayer = {
@@ -49,27 +50,33 @@ export default function BannerPrincipal({
     linkIframe = "https://www.youtube.com/embed/N48elyl_Qz8?si=mOpmNKT307wMo8wl"
 }: Props) {
 
+    // const [ modal, setModal ] = useState(false)
+
     return (
         <div>
-            <div>
+            <div class="relative bg-gradient-to-bl from-[#04A8EA] to-[#8D298F] ">
                 <Image 
                     src={imagem.src || ""} 
-                    width={100} 
-                    height={28} 
+                    width={1440} 
+                    height={500} 
                     alt={imagem.alt}
+                    class="relative"
                 />
-                <div>
-                    <Image
-                        src={imagemPlayer.src || ""} 
-                        width={100} 
-                        height={28} 
-                        alt={imagemPlayer.alt}
-                    /> 
-                    <h1>{textBanner}</h1>
-                    <a href={linkButton} target="_blank">{textButton}</a>
+                <div class="banner-main-home absolute mx-auto right-0 left-0">
+                    <div class="mx-auto text-center w-7/12 border-2 border-white py-14 px-8 relative">
+                        <h1 class="text-4xl font-family text-white w-full uppercase mb-6 font-bold">{textBanner}</h1>
+                        <Image
+                            src={imagemPlayer.src || ""} 
+                            width={100} 
+                            height={28} 
+                            alt={imagemPlayer.alt}
+                            class="mx-auto cursor-pointer my-10"
+                        /> 
+                        <a class="absolute -bottom-10 left-0 right-0 mx-auto w-1/5 bg-[#04A8EA] uppercase font-bold py-5 px-4 text-white" href={linkButton} target="_blank">{textButton}</a>
+                    </div>
                 </div>
             </div>
-            <div>
+            <div class="hidden">
                 <iframe src={linkIframe} loading="lazy"></iframe>    
             </div>
         </div>
