127fc226299cf957f4cd5e7b06f206879479f75e
Rodrigo Araujo <rodrigo.araujo@corebiz.ag>
2024-04-26 13:26:51 +0000

    Release Staging environment with 1 changes made by Rodrigo Araujo

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -2,45 +2,45 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $$$$$$$$$$$0 from "./apps/decohub.ts";
-import * as $$$$$$$$$$$1 from "./apps/site.ts";
-import * as $$$$$$0 from "./sections/BannerText.tsx";
-import * as $$$$$$1 from "./sections/BlogPost.tsx";
-import * as $$$$$$2 from "./sections/BlogPosts.tsx";
-import * as $$$$$$3 from "./sections/Faq.tsx";
-import * as $$$$$$4 from "./sections/Footer.tsx";
-import * as $$$$$$5 from "./sections/Header.tsx";
-import * as $$$$$$6 from "./sections/Hero.tsx";
-import * as $$$$$$7 from "./sections/ImageWithParagraph.tsx";
-import * as $$$$$$8 from "./sections/LastBannerHome.tsx";
-import * as $$$$$$9 from "./sections/Logos.tsx";
-import * as $$$$$$10 from "./sections/MainBanner.tsx";
-import * as $$$$$$11 from "./sections/MainPost.tsx";
-import * as $$$$$$12 from "./sections/NewsletterNew.tsx";
-import * as $$$$$$13 from "./sections/Testimonials.tsx";
-import * as $$$$$$14 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$$$$$$$0 from "./apps/decohub.ts";
+import * as $$$$$$$$$$$$1 from "./apps/site.ts";
+import * as $$$$$$$0 from "./sections/BannerText.tsx";
+import * as $$$$$$$1 from "./sections/BlogPost.tsx";
+import * as $$$$$$$2 from "./sections/BlogPosts.tsx";
+import * as $$$$$$$3 from "./sections/Faq.tsx";
+import * as $$$$$$$4 from "./sections/Footer.tsx";
+import * as $$$$$$$5 from "./sections/Header.tsx";
+import * as $$$$$$$6 from "./sections/Hero.tsx";
+import * as $$$$$$$7 from "./sections/ImageWithParagraph.tsx";
+import * as $$$$$$$8 from "./sections/LastBannerHome.tsx";
+import * as $$$$$$$9 from "./sections/Logos.tsx";
+import * as $$$$$$$10 from "./sections/MainBanner.tsx";
+import * as $$$$$$$11 from "./sections/MainPost.tsx";
+import * as $$$$$$$12 from "./sections/NewsletterNew.tsx";
+import * as $$$$$$$13 from "./sections/Testimonials.tsx";
+import * as $$$$$$$14 from "./sections/Theme/Theme.tsx";
 
 const manifest = {
   "sections": {
-    "site/sections/BannerText.tsx": $$$$$$0,
-    "site/sections/BlogPost.tsx": $$$$$$1,
-    "site/sections/BlogPosts.tsx": $$$$$$2,
-    "site/sections/Faq.tsx": $$$$$$3,
-    "site/sections/Footer.tsx": $$$$$$4,
-    "site/sections/Header.tsx": $$$$$$5,
-    "site/sections/Hero.tsx": $$$$$$6,
-    "site/sections/ImageWithParagraph.tsx": $$$$$$7,
-    "site/sections/LastBannerHome.tsx": $$$$$$8,
-    "site/sections/Logos.tsx": $$$$$$9,
-    "site/sections/MainBanner.tsx": $$$$$$10,
-    "site/sections/MainPost.tsx": $$$$$$11,
-    "site/sections/NewsletterNew.tsx": $$$$$$12,
-    "site/sections/Testimonials.tsx": $$$$$$13,
-    "site/sections/Theme/Theme.tsx": $$$$$$14,
+    "site/sections/BannerText.tsx": $$$$$$$0,
+    "site/sections/BlogPost.tsx": $$$$$$$1,
+    "site/sections/BlogPosts.tsx": $$$$$$$2,
+    "site/sections/Faq.tsx": $$$$$$$3,
+    "site/sections/Footer.tsx": $$$$$$$4,
+    "site/sections/Header.tsx": $$$$$$$5,
+    "site/sections/Hero.tsx": $$$$$$$6,
+    "site/sections/ImageWithParagraph.tsx": $$$$$$$7,
+    "site/sections/LastBannerHome.tsx": $$$$$$$8,
+    "site/sections/Logos.tsx": $$$$$$$9,
+    "site/sections/MainBanner.tsx": $$$$$$$10,
+    "site/sections/MainPost.tsx": $$$$$$$11,
+    "site/sections/NewsletterNew.tsx": $$$$$$$12,
+    "site/sections/Testimonials.tsx": $$$$$$$13,
+    "site/sections/Theme/Theme.tsx": $$$$$$$14,
   },
   "apps": {
-    "site/apps/decohub.ts": $$$$$$$$$$$0,
-    "site/apps/site.ts": $$$$$$$$$$$1,
+    "site/apps/decohub.ts": $$$$$$$$$$$$0,
+    "site/apps/site.ts": $$$$$$$$$$$$1,
   },
   "name": "site",
   "baseUrl": import.meta.url,
