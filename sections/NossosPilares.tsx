55ca8761fbcdf48b030f38716e1a664de1a0b27f
Rodrigo Araujo <rodrigo.eritel@gmail.com>
2024-05-01 00:32:40 -0300

    feat: create style pillar

--- sections/NossosPilares.tsx
+++ sections/NossosPilares.tsx
@@ -10,8 +10,23 @@ export interface Props {
     items?: ItemPillar[]
 }
 
+function validaCaracteres(strToReplace) {
+    const strSChar = "áàãâäéèêëíìîïóòõôöúùûüçÁÀÃÂÄÉÈÊËÍÌÎÏÓÒÕÖÔÚÙÛÜÇ";
+    const strNoSChars = "aaaaaeeeeiiiiooooouuuucAAAAAEEEEIIIIOOOOOUUUUC";
+    var newStr = "";
+    for (var i = 0; i < strToReplace.length; i++) {
+        if (strSChar.indexOf(strToReplace.charAt(i)) != -1) {
+            newStr += strNoSChars.substr(strSChar.search(strToReplace.substr(i, 1)), 1);
+        } else {
+            newStr += strToReplace.substr(i, 1);
+        }
+    }
+    
+    return newStr.replace(/[^a-zA-Z 0-9]/g, '').toUpperCase();
+}
+
 export default function NossosPilares({
-    title = "",
+    title = "Nossos pilares",
     items = [
         {
             title: "Missão",
@@ -32,17 +47,18 @@ export default function NossosPilares({
     ]
 }: Props) {
     return (
-        <div>
-            <h1>{title}</h1>
-            <div>
+        <div class="relative after:w-full after:h-96 after:bg-[#8F298C] after:absolute after:-bottom-12 after:-z-10 mb-20">
+            <h1 class="text-[#8D298F] text-4xl font-family text-center w-full uppercase font-bold mt-16 block mb-16">{title}</h1>
+            <div class="grid grid-cols-5 grid-rows-6 gap-4">
                 {
                     items.map((value) => (
-                        <div>
-                            <h4>{value.title}</h4>
+                        <div class={`${validaCaracteres(value.title).toLocaleLowerCase()} item-grid w-64 h-auto bg-[#DDF5FF] rounded-xl text-center flex flex-col items-center justify-center px-4 py-4`}>
+                            <h4 class="text-3xl font-bold bg-gradient-to-tr from-[#8D298F] to-[#04A8EA] inline-block text-transparent bg-clip-text mb-2">{value.title}</h4>
                             <div 
                                 dangerouslySetInnerHTML={{
                                     __html: value.content
                                 }}
+                                class="text-base"
                             />
                         </div>
                     ))
