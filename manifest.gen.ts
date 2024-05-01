1cc4b8105f24ff7b4c3229e8c61ab880475f0910
Rodrigo Araujo <rodrigo.eritel@gmail.com>
2024-05-01 17:18:06 -0300

    feat: add new page

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -2,53 +2,55 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $$$$$$$$$$$$0 from "./apps/decohub.ts";
-import * as $$$$$$$$$$$$1 from "./apps/site.ts";
-import * as $$$$$$$0 from "./sections/BannerPages.tsx";
-import * as $$$$$$$1 from "./sections/BannerText.tsx";
-import * as $$$$$$$2 from "./sections/BlogPost.tsx";
-import * as $$$$$$$3 from "./sections/BlogPosts.tsx";
-import * as $$$$$$$4 from "./sections/Faq.tsx";
-import * as $$$$$$$5 from "./sections/Footer.tsx";
-import * as $$$$$$$6 from "./sections/Founders.tsx";
-import * as $$$$$$$7 from "./sections/Header.tsx";
-import * as $$$$$$$8 from "./sections/Hero.tsx";
-import * as $$$$$$$9 from "./sections/ImageWithParagraph.tsx";
-import * as $$$$$$$10 from "./sections/LastBannerHome.tsx";
-import * as $$$$$$$11 from "./sections/Logos.tsx";
-import * as $$$$$$$12 from "./sections/MainBanner.tsx";
-import * as $$$$$$$13 from "./sections/MainPost.tsx";
-import * as $$$$$$$14 from "./sections/NewsletterNew.tsx";
-import * as $$$$$$$15 from "./sections/NossosPilares.tsx";
-import * as $$$$$$$16 from "./sections/RichText.tsx";
-import * as $$$$$$$17 from "./sections/Testimonials.tsx";
-import * as $$$$$$$18 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$$$$$$0 from "./apps/decohub.ts";
+import * as $$$$$$$$$$$1 from "./apps/site.ts";
+import * as $$$$$$0 from "./sections/BannerPages.tsx";
+import * as $$$$$$1 from "./sections/BannerText.tsx";
+import * as $$$$$$2 from "./sections/BlogPost.tsx";
+import * as $$$$$$3 from "./sections/BlogPosts.tsx";
+import * as $$$$$$4 from "./sections/Faq.tsx";
+import * as $$$$$$5 from "./sections/Footer.tsx";
+import * as $$$$$$6 from "./sections/Founders.tsx";
+import * as $$$$$$7 from "./sections/Header.tsx";
+import * as $$$$$$8 from "./sections/Hero.tsx";
+import * as $$$$$$9 from "./sections/ImageWithParagraph.tsx";
+import * as $$$$$$10 from "./sections/LastBannerHome.tsx";
+import * as $$$$$$11 from "./sections/Logos.tsx";
+import * as $$$$$$12 from "./sections/MainBanner.tsx";
+import * as $$$$$$13 from "./sections/MainPost.tsx";
+import * as $$$$$$14 from "./sections/Map.tsx";
+import * as $$$$$$15 from "./sections/NewsletterNew.tsx";
+import * as $$$$$$16 from "./sections/NossosPilares.tsx";
+import * as $$$$$$17 from "./sections/RichText.tsx";
+import * as $$$$$$18 from "./sections/Testimonials.tsx";
+import * as $$$$$$19 from "./sections/Theme/Theme.tsx";
 
 const manifest = {
   "sections": {
-    "site/sections/BannerPages.tsx": $$$$$$$0,
-    "site/sections/BannerText.tsx": $$$$$$$1,
-    "site/sections/BlogPost.tsx": $$$$$$$2,
-    "site/sections/BlogPosts.tsx": $$$$$$$3,
-    "site/sections/Faq.tsx": $$$$$$$4,
-    "site/sections/Footer.tsx": $$$$$$$5,
-    "site/sections/Founders.tsx": $$$$$$$6,
-    "site/sections/Header.tsx": $$$$$$$7,
-    "site/sections/Hero.tsx": $$$$$$$8,
-    "site/sections/ImageWithParagraph.tsx": $$$$$$$9,
-    "site/sections/LastBannerHome.tsx": $$$$$$$10,
-    "site/sections/Logos.tsx": $$$$$$$11,
-    "site/sections/MainBanner.tsx": $$$$$$$12,
-    "site/sections/MainPost.tsx": $$$$$$$13,
-    "site/sections/NewsletterNew.tsx": $$$$$$$14,
-    "site/sections/NossosPilares.tsx": $$$$$$$15,
-    "site/sections/RichText.tsx": $$$$$$$16,
-    "site/sections/Testimonials.tsx": $$$$$$$17,
-    "site/sections/Theme/Theme.tsx": $$$$$$$18,
+    "site/sections/BannerPages.tsx": $$$$$$0,
+    "site/sections/BannerText.tsx": $$$$$$1,
+    "site/sections/BlogPost.tsx": $$$$$$2,
+    "site/sections/BlogPosts.tsx": $$$$$$3,
+    "site/sections/Faq.tsx": $$$$$$4,
+    "site/sections/Footer.tsx": $$$$$$5,
+    "site/sections/Founders.tsx": $$$$$$6,
+    "site/sections/Header.tsx": $$$$$$7,
+    "site/sections/Hero.tsx": $$$$$$8,
+    "site/sections/ImageWithParagraph.tsx": $$$$$$9,
+    "site/sections/LastBannerHome.tsx": $$$$$$10,
+    "site/sections/Logos.tsx": $$$$$$11,
+    "site/sections/MainBanner.tsx": $$$$$$12,
+    "site/sections/MainPost.tsx": $$$$$$13,
+    "site/sections/Map.tsx": $$$$$$14,
+    "site/sections/NewsletterNew.tsx": $$$$$$15,
+    "site/sections/NossosPilares.tsx": $$$$$$16,
+    "site/sections/RichText.tsx": $$$$$$17,
+    "site/sections/Testimonials.tsx": $$$$$$18,
+    "site/sections/Theme/Theme.tsx": $$$$$$19,
   },
   "apps": {
-    "site/apps/decohub.ts": $$$$$$$$$$$$0,
-    "site/apps/site.ts": $$$$$$$$$$$$1,
+    "site/apps/decohub.ts": $$$$$$$$$$$0,
+    "site/apps/site.ts": $$$$$$$$$$$1,
   },
   "name": "site",
   "baseUrl": import.meta.url,
