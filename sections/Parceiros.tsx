2ff1d9d9f3bffad4cb8241007f408757c423d51d
Rodrigo Araujo <rodrigo.eritel@gmail.com>
2024-05-06 13:01:31 -0300

    feat: add island - participar

--- sections/Parceiros.tsx
+++ sections/Parceiros.tsx
@@ -26,18 +26,20 @@ export default function Parceiros({
 }: Props) {
     return (
         <div>
-            <h1 class="text-[#8D298F] text-4xl font-family text-center w-full uppercase font-bold mt-16 block mb-16">{title}</h1>
-            {
-                logoParceiro?.map((value) => (
-                    <Image 
-                        src={value.src || ""} 
-                        width={285} 
-                        height={98} 
-                        alt={value.label}
-                        class="relative"
-                    />
-                ))
-            }
+            <h1 class="text-[#8D298F] text-5xl font-family text-center w-full uppercase mb-6 font-bold after:h-2 after:w-[30%] after:block after:mx-auto after:bg-gradient-to-r after:from-[#8D298F] after:to-[#04A8EA] after:mt-1">{title}</h1>
+            <div class="flex flex-wrap w-1/2 mx-auto justify-around">
+                {
+                    logoParceiro?.map((value) => (
+                        <Image 
+                            src={value.src || ""} 
+                            width={285} 
+                            height={98} 
+                            alt={value.label}
+                            class="relative mb-8"
+                        />
+                    ))
+                }
+            </div>
         </div>
     )
 }
